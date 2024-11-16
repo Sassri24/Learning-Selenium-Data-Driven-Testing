1eab2c989018f6bf0bfdbfe1a387e065645b2512
Sassri Chamod <116500146+Sassri24@users.noreply.github.com>
2024-11-16 22:39:40 +0530

    create folder structure

--- src/test/java/datadriventesting.java
+++ src/test/java/datadriventesting.java
@@ -5,7 +5,6 @@ public class datadriventesting {
     public static void main(String [] args){
         System.out.println("test run");
         System.out.println("test run2");
+        System.out.println("test run3");
     }
-
-
 }
