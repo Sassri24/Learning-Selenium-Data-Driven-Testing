f3057e41d491f27b5188c8747f9bff23d965d774
Sassri Chamod <116500146+Sassri24@users.noreply.github.com>
2024-11-23 00:31:26 +0530

    Update datadriventesting.java

--- src/test/java/datadriventesting.java
+++ src/test/java/datadriventesting.java
@@ -7,5 +7,6 @@ public class datadriventesting {
         System.out.println("test run3");
         System.out.println("test run4");
         System.out.println("test run5");
+        System.out.println("test run6");
     }
 }
